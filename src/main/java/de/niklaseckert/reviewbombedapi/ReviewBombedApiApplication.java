b42bc3b698ce409d9fcceefff9ace46632aefec2
niklaseckert <niklas.eckert.1807@gmail.com>
2022-01-30 18:51:57 +0100

    Initial commit.

---
+++ src/main/java/de/niklaseckert/reviewbombedapi/ReviewBombedApiApplication.java
@@ -0,0 +1,13 @@
+package de.niklaseckert.reviewbombedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewBombedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewBombedApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/niklaseckert/reviewbombedapi/ReviewBombedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.niklaseckert.reviewbombedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewBombedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
